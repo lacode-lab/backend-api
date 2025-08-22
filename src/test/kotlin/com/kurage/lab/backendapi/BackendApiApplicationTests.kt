0425c235654397c10b145f5da1a7babc71ef391a
mac-yuuichi <orangeeeee.ore.ore@gmail.com>
2025-08-22 19:15:25 +0900

    first commit

---
+++ src/test/kotlin/com/kurage/lab/backendapi/BackendApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kurage.lab.backendapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
